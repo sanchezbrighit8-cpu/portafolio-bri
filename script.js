f1b3ff667edc7db29774808c5af09c0eae8f2380
jeryroldan <jeffersob2815@gmail.com>
2026-03-27 20:05:35 -0500

    primer comentario

---
+++ script.js
@@ -0,0 +1,8 @@
+function abrirModal(src) {
+  document.getElementById('modal').classList.add('active');
+  document.getElementById('imagenModal').src = src;
+}
+
+function cerrarModal() {
+  document.getElementById('modal').classList.remove('active');
+}
